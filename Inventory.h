5a158ffa031d0672f6223161283e0d1f24e20228
CoffeeStrike <omniexpansiontheory@gmail.com>
2025-05-11 19:42:22 +0800

    Add files via upload

--- Inventory.h
+++ Inventory.h
@@ -379,7 +379,9 @@ class Inventory{
                     }
                 }
             }
-                   
+           
+		   
+		           
             cout << "SORTED INVENTORY" << endl;
             cout << "==================================" << endl;
             cout << "Name\t\tID\t\tQuantity\t\tType\t\tPrice\t\tCategory" << endl;
